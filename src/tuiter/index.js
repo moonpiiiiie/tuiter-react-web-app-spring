f167908b320b09ec76386d53f19ad614e9baa418
moonpiiiiie <cheng.xue910@gmail.com>
2023-03-05 16:51:56 -0800

    fix deploy error

--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -26,7 +26,6 @@ import WhoToFollowList from "./who-to-follow-list";
 
 function Tuiter() {
  return (
-
     <div className="row mt-2">
       <div className="col-2 col-md-2 col-lg-1 col-xl-2">
         <NavigationSidebar active="explore"/>
